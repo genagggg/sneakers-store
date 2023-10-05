ab550882cafb11eb6a113108c60f6db196aa625e
Gena <mr.genag@mail.ru>
2023-10-05 16:23:32 +0500

    Header create component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './scss/app.scss'
+import Header from './components/Header'
 
 function App() {
   return (
     <div class="wrapper">
-   
+   <Header/>
     <div class="content">
       <div class="container">
         <div class="content__top">
