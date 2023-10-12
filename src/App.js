104fd136fcfcfd910378f17449787910d0a86a75
Gena <mr.genag@mail.ru>
2023-10-12 20:06:34 +0500

    Извлёк данные с бекэнда

--- src/App.js
+++ src/App.js
@@ -1,31 +1,41 @@
-import './scss/app.scss'
-import Header from './components/Header'
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
-import pizzas from './assets/pizzas.json';
-
-console.log(pizzas)
+import React from "react";
+import "./scss/app.scss";
+import Header from "./components/Header";
+import Categories from "./components/Categories";
+import Sort from "./components/Sort";
+import PizzaBlock from "./components/PizzaBlock";
 
 function App() {
+  const [items, setItems] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch(`https://65279f93931d71583df116d9.mockapi.io/items`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((json) => {
+        setItems(json);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
-   <Header/>
-    <div className="content">
-      <div className="container">
-        <div className="content__top">
-          <Categories />
-          <Sort />
-        </div>
-        <h2 className="content__title">Все пиццы</h2>
-        <div className="content__items">
-          {pizzas.map((obj)=>(
-            <PizzaBlock key={obj.id} {...obj}/>
-          ))}
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {items.map((obj) => (
+              <PizzaBlock key={obj.id} {...obj} />
+            ))}
+          </div>
         </div>
       </div>
     </div>
-  </div>
   );
 }
 
