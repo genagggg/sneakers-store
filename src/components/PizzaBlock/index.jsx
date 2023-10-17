2f353dbb8c7ca4e4399304211339cfa96087b98d
Gena <mr.genag@mail.ru>
2023-10-17 20:50:29 +0500

    адаптив Блока Пиццы

--- src/components/PizzaBlock/index.jsx
+++ src/components/PizzaBlock/index.jsx
@@ -6,7 +6,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
 
   const typesName = ["тонкое", "традиционное"];
   return (
-    <div className="pizza-block-wrapper">
+    <div className="pizza-block__wrapper">
     <div className="pizza-block">
       <img className="pizza-block__image" src={imageUrl} alt="Pizza" />
       <h4 className="pizza-block__title">{title}</h4>
