bde19a7f31cd1b8d9cc789c94ba63afae1a99179
Gena <mr.genag@mail.ru>
2023-10-23 20:59:30 +0500

    Выполнил поиск на фронтенде

--- src/App.js
+++ src/App.js
@@ -9,14 +9,14 @@ import { Routes, Route } from "react-router-dom";
 function App() {
 
   const [searchValue, setSearchValue] = React.useState('');
-
+console.log(searchValue, "kkkk")
   return (
     <div className="wrapper">
       <Header searchValue={searchValue} setSearchValue={setSearchValue}/>
       <div className="content">
         
           <Routes>
-            <Route path="/" element={<Home/>}/>
+            <Route path="/" element={<Home searchValue={searchValue}/>}/>
             <Route path="/cart" element={<Cart/>} />
             <Route path="*" element={<NotFound/>} />
           </Routes>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import ButtonList from "../components/ButtonList";
 
-const Home = () => {
+const Home = ({ searchValue }) => {
   const [items, setItems] = React.useState([]);
 
   const [isLoading, setIsLoading] = React.useState(true);
@@ -15,19 +15,34 @@ const Home = () => {
 
   const [sortType, setSortType] = React.useState({
     name: "популярности",
-    sortProperty: "rating"
+    sortProperty: "rating",
   });
 
-  const [checkItem, setCheckItem] = React.useState(0)
+  const [checkItem, setCheckItem] = React.useState(0);
+  
+  const pizzas = items
+  .filter((obj) => {
+    if (obj.title.toLowerCase().includes(searchValue.toLowerCase())) {
+      return true;
+    }
+
+    return false;
+  }).map((obj) => <PizzaBlock key={obj.id} {...obj} />);
+
+const skeletons = [...new Array(6)].map((_, index) => (
+  <Skeleton key={index} />
+));
 
   React.useEffect(() => {
-    setIsLoading(true)
+    setIsLoading(true);
 
-    const sortBy = sortType.sortProperty.replace('-', '');
-    const order = sortType.sortProperty.includes('-')?'asc':'desc';
-    const category = categoryId > 0 ? `category=${categoryId}`:'';
+    const sortBy = sortType.sortProperty.replace("-", "");
+    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const category = categoryId > 0 ? `category=${categoryId}` : "";
 
-    fetch(`https://65279f93931d71583df116d9.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`)
+    fetch(
+      `https://65279f93931d71583df116d9.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`
+    )
       .then((res) => {
         return res.json();
       })
@@ -35,23 +50,25 @@ const Home = () => {
         setItems(json);
         setIsLoading(false);
       });
-      window.scrollTo(0,0)
+    window.scrollTo(0, 0);
   }, [categoryId, sortType]);
-console.log(categoryId, sortType)
+  console.log(categoryId, sortType);
+
+ 
+
   return (
     <>
-    <div className="container">
-      <ButtonList value={checkItem} onClickButton={(i)=>setCheckItem(i)}/>
-      <div className="content__top">
-        <Categories value={categoryId} onClickCategory={(i)=>setCategoryId(i)}/>
-        <Sort value={sortType} onClickSort={(i)=>setSortType(i)}/>
-      </div>
-      <h2 className="content__title">Все пиццы</h2>
-      <div className="content__items">
-        {isLoading
-          ? [...new Array(6)].map((_, index) => <Skeleton key={index} />)
-          : items.map((obj) => <PizzaBlock key={obj.id} {...obj} />)}
-      </div>
+      <div className="container">
+        <ButtonList value={checkItem} onClickButton={(i) => setCheckItem(i)} />
+        <div className="content__top">
+          <Categories
+            value={categoryId}
+            onClickCategory={(i) => setCategoryId(i)}
+          />
+          <Sort value={sortType} onClickSort={(i) => setSortType(i)} />
+        </div>
+        <h2 className="content__title">Все пиццы</h2>
+        <div className="content__items">{isLoading ? skeletons : pizzas}</div>
       </div>
     </>
   );
