07cb6beb8ec2fa0ee15fbe64626b171f06a715ee
genaggg <mr.genag@mail.ru>
2023-11-15 20:43:17 +0500

    Задача feat-ACR-14 С делать debounce инпута

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,16 +1,29 @@
 import React from "react";
+import debounce from "lodash.debounce";
 import style from "./Search.module.scss";
 import { SearchContext } from "../../App";
 
 const Search = () => {
+  const [value, setValue] = React.useState();
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
+  const inputRef = React.useRef();
 
- const {searchValue, setSearchValue} = React.useContext(SearchContext);
-const inputRef = React.useRef()
+  const onClicClear = () => {
+    setSearchValue("");
+    setValue("");
+    inputRef.current.focus();
+  };
 
-const onClicClear =()=>{
-  setSearchValue('')
-  inputRef.current.focus()
-}
+  const updateSearchValue = React.useCallback(
+    debounce((str) => {
+      setSearchValue(str);
+    }, 1000),
+    []
+  );
+  const onChangeInput = (event) => {
+    setValue(event.target.value);
+    updateSearchValue(event.target.value);
+  };
 
   return (
     <div className={style.root}>
@@ -27,19 +40,27 @@ const onClicClear =()=>{
       </svg>
       <input
         className={style.input}
-        value={searchValue}
+        value={value}
         ref={inputRef}
-        onChange={(e) => setSearchValue(e.target.value)}
+        onChange={onChangeInput}
         type="text"
         placeholder="Поиск пиццы..."
       />
 
-     { searchValue && <svg onClick={onClicClear} className={style.clearIcon} version="1.1" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-        <g id="grid_system" />
-        <g id="_icons">
-          <path d="M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z" />
-        </g>
-      </svg>}
+      {searchValue && (
+        <svg
+          onClick={onClicClear}
+          className={style.clearIcon}
+          version="1.1"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <g id="grid_system" />
+          <g id="_icons">
+            <path d="M5.3,18.7C5.5,18.9,5.7,19,6,19s0.5-0.1,0.7-0.3l5.3-5.3l5.3,5.3c0.2,0.2,0.5,0.3,0.7,0.3s0.5-0.1,0.7-0.3   c0.4-0.4,0.4-1,0-1.4L13.4,12l5.3-5.3c0.4-0.4,0.4-1,0-1.4s-1-0.4-1.4,0L12,10.6L6.7,5.3c-0.4-0.4-1-0.4-1.4,0s-0.4,1,0,1.4   l5.3,5.3l-5.3,5.3C4.9,17.7,4.9,18.3,5.3,18.7z" />
+          </g>
+        </svg>
+      )}
     </div>
   );
 };
--- src/redux/slises/filterSlice.js
+++ src/redux/slises/filterSlice.js
@@ -1,27 +1,26 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-
-   categoryId:0,
-   sort:{
-    name: 'популярности',
-    sortProperty: 'rating'
-   }
-   
-  }
+  categoryId: 0,
+  pageCount: 1,
+  sort: {
+    name: "популярности",
+    sortProperty: "rating",
+  },
+};
 
 const filterSlice = createSlice({
-  name: 'filters',
+  name: "filters",
   initialState,
   reducers: {
     setCategoryId(state, action) {
-      state.categoryId = action.payload
+      state.categoryId = action.payload;
     },
     setSort(state, action) {
-      state.sort = action.payload
+      state.sort = action.payload;
     },
   },
-})
+});
 
-export const { setCategoryId, setSort } = filterSlice.actions
-export default filterSlice.reducer
+export const { setCategoryId, setSort } = filterSlice.actions;
+export default filterSlice.reducer;
