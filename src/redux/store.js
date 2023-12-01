00721e206ea32c98313994f446b459e61b7dd1bc
genaggg <mr.genag@mail.ru>
2023-12-01 20:12:59 +0500

    feat-PizzassApp-4562 Сделать динамически наполняемую корзину

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,11 @@
 import { combineReducers, createStore } from "@reduxjs/toolkit";
 import filter from "./slises/filterSlice";
+import cart from "./slises/cartSlice";
 import { composeWithDevTools } from "redux-devtools-extension";
 
 export const store = createStore(
   combineReducers({
     filter,
+    cart,
   }), composeWithDevTools()
 );
